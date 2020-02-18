08901dc1169269dab75a7d3359322a75e42e4e0c
Eonm <eon.mathis@gmail.com>
2020-02-18 22:32:44 +0100

    Update example with fmt::Display

--- examples/show_interfaces.rs
+++ examples/show_interfaces.rs
@@ -1,13 +1,12 @@
 //! Pretty print host interfaces.
 
 use nl80211::Socket;
-use nl80211::PrettyFormat;
 
 fn main() -> Result<(), neli::err::NlError> {
     let interfaces = Socket::connect()?.get_interfaces_info()?;
 
     for interface in interfaces {
-        println!("{}\n", interface.pretty_format());
+        println!("{}\n", interface);
     }
 
     Ok(())
